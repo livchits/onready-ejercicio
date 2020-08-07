d2682a1f9273970329f903f9cdbb6c69d712fa90
Lucas Livchits <livchitsl@gmail.com>
2020-08-07 13:09:45 -0300

    Add vehicles data

---
+++ vehiclesList.js
@@ -0,0 +1,6 @@
+const bd = [
+  { brand: 'Peugeot', model: '206', doors: 4, price: 200000 },
+  { brand: 'Honda', model: 'Titan', displacement: 125, price: 60000 },
+  { brand: 'Peugeot', model: '208', doors: 5, price: 250000 },
+  { brand: 'Yamaha', model: 'YBR', displacement: 160, price: 80500.5 },
+];
