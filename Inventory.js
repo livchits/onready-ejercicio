189fba4c38ee258c036c2d2a49c6e543cb80a8c3
Lucas Livchits <livchitsl@gmail.com>
2020-08-07 13:14:28 -0300

    Add getter for prices

--- Inventory.js
+++ Inventory.js
@@ -2,4 +2,9 @@ class Inventory {
   constructor(productsList) {
     this._products = productsList;
   }
+
+  get prices() {
+    const priceList = this._products.map((product) => product.price);
+    return priceList;
+  }
 }
