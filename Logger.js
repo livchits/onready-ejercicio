599e7a7866ef319547e9ae96525124614d09cd0e
Lucas Livchits <livchitsl@gmail.com>
2020-08-07 13:25:29 -0300

    Add method to log all products

--- Logger.js
+++ Logger.js
@@ -2,4 +2,15 @@ class Logger {
   constructor(inventory) {
     this.inventory = inventory;
   }
+
+  logAllProducts() {
+    const allProductsData = this.inventory._products.reduce(
+      (dataToPrint, product) => {
+        dataToPrint += this.inventory.getProductData(product) + '\n';
+        return dataToPrint;
+      },
+      ''
+    );
+    console.log(`${allProductsData}=============================`);
+  }
 }
