c880f046b6e5a81ee28984a9cd090dd21485b12c
Lucas Livchits <livchitsl@gmail.com>
2020-08-07 13:15:36 -0300

    Add method to get product data

--- Inventory.js
+++ Inventory.js
@@ -1,3 +1,6 @@
+const Car = require('./Car');
+const Motorcycle = require('./Motorcycle');
+
 class Inventory {
   constructor(productsList) {
     this._products = productsList;
@@ -6,5 +9,23 @@ class Inventory {
   get prices() {
     const priceList = this._products.map((product) => product.price);
     return priceList;
+  }
+
+  getProductData(product) {
+    const { brand, model, doors, displacement, price } = product;
+    let productData;
+
+    if (product instanceof Car) {
+      productData = `Marca: ${brand} // Modelo: ${model} // Puertas: ${doors} // Precio: ${formatPrice(
+        price
+      )}`;
+    }
+    if (product instanceof Motorcycle) {
+      productData = `Marca: ${brand} // Modelo: ${model} // Cilindrada: ${formatDisplacement(
+        displacement
+      )} // Precio: ${formatPrice(price)}`;
+    }
+
+    return productData;
   }
 }
