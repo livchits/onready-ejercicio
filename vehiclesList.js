96204965f10ddf574db84fc6f95f58585cc3a6ed
Lucas Livchits <livchitsl@gmail.com>
2020-08-07 13:12:06 -0300

    Create and export list of vehicles

--- vehiclesList.js
+++ vehiclesList.js
@@ -1,6 +1,17 @@
+const Car = require('./Car');
+const Motorcycle = require('./Motorcycle');
+
 const bd = [
   { brand: 'Peugeot', model: '206', doors: 4, price: 200000 },
   { brand: 'Honda', model: 'Titan', displacement: 125, price: 60000 },
   { brand: 'Peugeot', model: '208', doors: 5, price: 250000 },
   { brand: 'Yamaha', model: 'YBR', displacement: 160, price: 80500.5 },
 ];
+
+function createVehicle(vehicle) {
+  return vehicle.doors ? new Car(vehicle) : new Motorcycle(vehicle);
+}
+
+const vehiclesLits = bd.map(createVehicle);
+
+module.exports = vehiclesLits;
