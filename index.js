6544ae24a91fefc3153c5b813195fdecad66be76
Lucas Livchits <livchitsl@gmail.com>
2020-08-07 13:39:32 -0300

    Create logger

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const vehiclesList = require('./vehiclesList');
 const Inventory = require('./Inventory');
+const Logger = require('./Logger');
 
 const vehiclesInventory = new Inventory(vehiclesList);
+
+const vehiclesLogger = new Logger(vehiclesInventory);
