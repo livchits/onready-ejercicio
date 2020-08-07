4a900d4e79dcecda2050f7d4824360b287b8bdb8
Lucas Livchits <livchitsl@gmail.com>
2020-08-07 13:22:08 -0300

    Add function to format price

--- utils.js
+++ utils.js
@@ -2,4 +2,20 @@ function formatDisplacement(displacement) {
   return `${displacement}c`;
 }
 
-module.exports = { formatDisplacement };
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  currencyDisplay: 'narrowSymbol',
+  minimumFractionDigits: 2,
+});
+
+//No es la forma más elegante, pero es una alternativa dado que la internacionalización no se comporta del mismo modo en Node que en el browser. Otras posibles soluciones son las mencionadas aquí (https://stackoverflow.com/a/55183777/11660703), pero no se ajustan a los requerimientos del ejercicio
+function formatPrice(price) {
+  const priceInCurencyFormat = formatter.format(price);
+  const priceChars = priceInCurencyFormat.split(',').join('.').split('');
+  priceChars.splice(-3, 1, ',');
+  const priceFromated = priceChars.join('');
+  return priceFromated;
+}
+
+module.exports = { formatDisplacement, formatPrice };
