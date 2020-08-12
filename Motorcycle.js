7d1df76d38556bba3a869ee360d2c0af627beda8
Lucas Livchits <livchitsl@gmail.com>
2020-08-12 12:51:43 -0300

    Add getter for data

--- Motorcycle.js
+++ Motorcycle.js
@@ -1,9 +1,18 @@
 const Vehicle = require('./Vehicle');
+const { formatDisplacement, formatPrice } = require('./utils');
 
 class Motorcycle extends Vehicle {
   constructor({ brand, model, displacement, price }) {
     super({ brand, model, price });
     this.displacement = displacement;
+  }
+  get data() {
+    const motorcycleData = `Marca: ${this.brand} // Modelo: ${
+      this.model
+    } // Cilindrada: ${formatDisplacement(
+      this.displacement
+    )} // Precio: ${formatPrice(this.price)}`;
+    return motorcycleData;
   }
 }
 
