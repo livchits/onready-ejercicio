8cd63bd7880507d04576abbff833d847a9aa58e7
Lucas Livchits <livchitsl@gmail.com>
2020-08-12 12:49:27 -0300

    Add getter for data

--- Car.js
+++ Car.js
@@ -1,9 +1,17 @@
 const Vehicle = require('./Vehicle');
+const { formatPrice } = require('./utils');
 
 class Car extends Vehicle {
   constructor({ brand, model, doors, price }) {
     super({ brand, model, price });
     this.doors = doors;
+  }
+
+  get data() {
+    const carData = `Marca: ${this.brand} // Modelo: ${
+      this.model
+    } // Puertas: ${this.doors} // Precio: ${formatPrice(this.price)}`;
+    return carData;
   }
 }
 
